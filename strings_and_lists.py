663a9f2f7593571110c831a9b47615423ba03d78
Katie Thornton <katie.thornton07@gmail.com>
2026-03-18 18:16:45 +0000

    Add files via upload
    Learning to slice strings in python

---
+++ strings_and_lists.py
@@ -0,0 +1,8 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> s = "jDCIiph35wiRmHtMkUgKB15TfHIHPpComzjcuYwG6ILbFdGmjinjEquDQuVJo2pgKaKFWbhGybjF8PpOm2NHCLvnyuxRXEubalaena36OiLKPoEfBvr2R4JNjhassanicaUxBCRjhFKu08adP1Exv0ZTXZFOBQBjpTfvinNJNeGwhMxJWDT8Sazz2ZX1."
+>>> a,b,c,d = 93, 101, 121, 129
+>>> slice1= s[a : b+1]
+>>> slice2= s[c : d+1]
+>>> print(slice1 + " " + slice2)
+Eubalaena hassanica
